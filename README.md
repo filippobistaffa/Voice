ea7152098d3c2f9d48bff36991830d478afd203c
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-10-16 14:47:00 +0200

    Run script

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-Solve the GCCF problem with IDM property
+Solve the GCCF problem on ISGs
 ===================
+
 This is an implementation of the algorithm presented by Thomas Voice, Maria Polukarov, and Nicholas R. Jennings in “[Coalition Structure Generation over Graphs](https://www.jair.org/media/3715/live-3715-6552-jair.pdf)”, Journal of Artificial Intelligence Research (JAIR), pages 165-196, 2012.
+
+Execution
+----------
+Voice must be executed by means of the [`voice.sh`](voice.sh) script, i.e.,
+```
+./voice.sh -i <filename> -o <filename>
+
+-g	Input graph filename
+-p	Outputs solution to file
+```
 
 Acknowledgements
 ----------
---
+++ voice.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+red='\033[0;31m'			# Red
+nc='\033[0m'				# No color
+re='^[0-9]+$'				# Regular expression to detect natural numbers
+
+usage() { echo -e "Usage: $0 -i <filename> [-o <filename>]\n-i\tInput graph filename\n-o\tOutputs solution to file (optional)" 1>&2; exit 1; }
+
+while getopts ":i:o:" o; do
+	case "${o}" in
+	i)
+		i=${OPTARG}
+		if [ ! -f "$i" ]
+		then
+			echo -e "${red}Input file \"$i\" not found!${nc}\n" 1>&2; 
+			usage
+		fi
+		;;
+	o)
+		out=${OPTARG}
+		touch $out 2> /dev/null
+		rc=$?
+		if [[ $rc != 0 ]]
+		then
+			echo -e "${red}Unable to create $out${nc}" 1>&2; 
+			exit
+		else
+			rm $out
+		fi
+		;;
+	\?)
+		echo -e "${red}-$OPTARG is not a valid option!${nc}\n" 1>&2; 
+		usage
+		;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${i}" ]; then
+	echo -e "${red}Required parameter -i missing${nc}\n" 1>&2; 
+	usage
+fi
+
+tmp=`mktemp`
+
+e=`cat "$i" | wc -l`
+n=`grep -v " " "$i" | wc -l`
+e=$(( $e - $n ))
+echo "#define E $e" > $tmp
+echo "#define N $n" >> $tmp
+
+if [ ! -z $out ]
+then
+	echo "#define SOL \"$out\"" >> $tmp
+fi
+
+if [ ! -f instance.h ]
+then
+	mv $tmp "instance.h"
+else
+	md5a=`md5sum instance.h | cut -d\  -f 1`
+	md5b=`md5sum $tmp | cut -d\  -f 1`
+
+	if [ $md5a != $md5b ]
+	then
+		mv $tmp "instance.h"
+	else
+		rm $tmp
+	fi
+fi
+
+#make -j
+
+if [[ $? == 0 ]]
+then
+	bin=$0
+	bin=${bin%???}
+	#$bin $i
+fi
